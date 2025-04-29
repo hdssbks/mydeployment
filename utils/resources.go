b9370f47c7493fb8320e6709581b3ac2cb9ef0f2
hdssbks <317228455@qq.com>
2025-04-29 16:34:04 +0800

    update

--- utils/resources.go
+++ utils/resources.go
@@ -3,21 +3,13 @@ package utils
 import (
 	"bytes"
 	"fmt"
-	"log"
-	"math/rand"
-	"strings"
-	"text/template"
-	"time"
-
 	"github.com/hdssbks/mydeployment/api/v1beta1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/yaml"
+	"log"
+	"strings"
+	"text/template"
 )
-
-func init() {
-	// 初始化全局随机数生成器
-	rand.New(rand.NewSource(time.Now().UnixNano()))
-}
 
 var funcMap = template.FuncMap{
 	"randAlphaNum": RandStr,
@@ -32,13 +24,13 @@ func NewPod(deployment *v1beta1.MyDeployment) *corev1.Pod {
 	}
 
 	buffer := bytes.Buffer{}
-	if err = tpl.Execute(&buffer, deployment); err != nil {
+	if err := tpl.Execute(&buffer, deployment); err != nil {
 		log.Fatalln(fmt.Errorf("error excute template: %v", err))
 	}
 
 	err = yaml.Unmarshal(buffer.Bytes(), pod)
 	if err != nil {
-		log.Fatalln(fmt.Errorf("unable to unmarshal template: %v", err))
+		panic("unable to unmarshal template")
 	}
 	return pod
 }
